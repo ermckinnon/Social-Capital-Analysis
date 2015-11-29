a4cdeac89d24a8794c140917af1c45edb4ef047c
unknown <ewen.mckinnon@CO-C001321.ad.cabinetoffice.gov.uk>
2015-11-29 23:00:48 +0000

    Updated Worlbank load

--- Social Capital Analysis.R
+++ Social Capital Analysis.R
@@ -1,2 +1,22 @@
-setwd("C:/Social Capital")
+setwd("C:/Social Capital/Social Capital Analysis") #set a working directory for data - this directory needs to pre-exist
+#Download file and put in working directory
+#fileurl <- "http://data.worldbank.org/sites/default/files/total_and_per_capita_wealth_of_nations.xls"
+#download.file(fileurl, 
+#             destfile="./total_and_per_capita_wealth_of_nations.xls")
 
+#convert file to dataframe
+library(readxl)
+readdata<-read_excel("total_and_per_capita_wealth_of_nations.xls",
+                     sheet = "total wealth, 2005",na = c(".."))
+WBdata_2005 <- data.frame(readdata[6:214,1:10])
+colnames(WBdata_2005) <- readdata[5,1:10]
+rownames(WBdata_2005) <- NULL
+WBdata_2005[, c(5:10)] <- sapply(WBdata_2005[, c(5:10)], as.numeric)
+
+
+str(WBdata_2005)
+
+
+#To update Readmefile
+library(knitr)
+knit('readme.Rmd')
